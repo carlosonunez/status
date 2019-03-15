298e340e80e6f41cb03365f8c76cda539270d07f
Carlos Nunez <carlosonunez@noreply.github.com>
2019-03-14 20:18:22 -0400

    Reorganize

---
+++ features/step_definitions/signing_up.rb
@@ -0,0 +1,28 @@
+Given("a Slack client") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I start a chat with {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I type {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am asked for my TripIt API key") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am asked for an initial filter") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("my status should be updated within {int} seconds.") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see a private message saying {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
